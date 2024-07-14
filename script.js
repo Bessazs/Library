bf658216c644dfd82267297a946757fc09ff28d0
Bessa <bessa.vitor06@gmail.com>
2024-07-14 04:25:52 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+myLibrary = [];
+
+
+function book(title, author, pages, read){
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+}
+
+
+
+function addBookToLibrary() {
+  
+}
